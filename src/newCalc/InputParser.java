961ed53237b32e6884ce2c0cbbd91c2639e89379
i311648 <asya.chakarova@sap.com>
2018-08-31 12:30:07 +0300

    new test, changes in parser - token formatting

--- src/newCalc/InputParser.java
+++ src/newCalc/InputParser.java
@@ -11,6 +11,12 @@ public class InputParser  {
 	Number operand1;
 	Number operand2;
 	
+	int UNKNOWN = -1;
+	int NUMBER = 0;
+	int OPERATOR = 1;
+	int LEFT_PARENTHESIS = 2;
+	int RIGHT_PARENTHESIS = 3;
+	
 	public InputParser(){
 		operatorStack = new TokenStack();
 		valueStack = new TokenStack();
@@ -33,26 +39,29 @@ public class InputParser  {
 		} catch (Exception e){ System.err.println(e);}
 		return joinedString;
 	}
+	public static String[] formatStringToStrArr(String joinedString){
+			String[] tokensInParts = joinedString.split("(?<=[-+*x/()])|(?=[-+*x/()])"); //{"1","+","2","-","3"}
+		return tokensInParts;
+	}
 	
 	public Expression parse(String[] inputArgs){
-		String joinedString = formatingJoinedString(inputArgs);
-        Token t = new Token();
-        		System.out.println(joinedString);
-		//String[] parts = inputString.split(" ");
-		String[] tokens = joinedString.split("(?<=[-+*x/()])|(?=[-+*x/()])");
-		//Token[] tokens = new Token[joinedString.length()];
 		
-		//TODO
+		String joinedString = formatingJoinedString(inputArgs); //1+2-3
+		String[] tokensInParts = formatStringToStrArr(joinedString);
+		Token[] tokens = new Token[tokensInParts.length];
+
 		for (int i = 0; i < tokens.length; i++){
-			tokens[i] = new Token(parts[i]);
+			tokens[i] = new Token(tokensInParts[i]);
 		}
+		
+		Token t = new Token();
 		
 		for (int index = 0; index < tokens.length; index++){
 			Token nextT = tokens[index];
-			
-			if (nextT.getType().equals(0)){
+			//TODO test this part
+			if (nextT.getType() == NUMBER){ 
 				valueStack.push(nextT);
-			} else if (nextT.getType().equals("OPERATOR")){
+			} else if (nextT.getType() == OPERATOR){
 				if (operatorStack.isEmpty() || nextT.getPrio() > operatorStack.top().getPrio()) {
                     operatorStack.push(nextT);
 				
@@ -65,17 +74,17 @@ public class InputParser  {
 						//pass it to Token.evaluate
 					}
 				}
-			} else if (nextT.getType().equals("LEFT_PARENTHESIS")){
+			} else if (nextT.getType() == LEFT_PARENTHESIS){
 				operatorStack.push(nextT);
-			} else if (nextT.getType().equals("RIGHT_PARENTHESIS")){
-				while (!operatorStack.isEmpty() && operatorStack.top().getType().equals("OPERATOR")) {
+			} else if (nextT.getType() == RIGHT_PARENTHESIS){
+				while (!operatorStack.isEmpty() && (operatorStack.top().getType() == OPERATOR)) {
                     Token tokenToProcess = operatorStack.top();
                     operatorStack.pop();
                     valueStackCollection();
                     t.evaluate(operand1, operand2);
                   //pass it to Token.evaluate
                 }
-				if (!operatorStack.isEmpty() && operatorStack.top().getType().equals("LEFT_PARENTHESIS")) {
+				if (!operatorStack.isEmpty() && (operatorStack.top().getType() == LEFT_PARENTHESIS)) {
                     operatorStack.pop();
                     valueStackCollection(); 
                     t.evaluate(operand1, operand2);
--- test/newCalc/TokenTest.java
+++ test/newCalc/TokenTest.java
@@ -1,11 +1,8 @@
 package newCalc;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
 import org.junit.Test;
-import static org.hamcrest.MatcherAssert.assertThat;
-
 
 public class TokenTest {
 	
@@ -22,38 +19,50 @@ public class TokenTest {
 		
 		TokenStack ts = new TokenStack();
 		ts.push(t1); ts.push(t2); ts.push(t3);
-		
-		assertTrue(ts.equals(inputTokens));
+
 		ts.pop();
 		Token token = ts.top();
 		assertTrue( token.equals(inputTokens.indexOf(inputTokens.size()-1)) );
 	}
 	
 	@Test 
-	public void testInputParser_ReturnTokenArr(){
-		String[] args = {"1+", "2","-"," ","3"};
-		//split + trim : 
-		String inputString = null;
-		inputString = InputParser.createInputString(args); //"1+2-3"
+	public void matchElements(){
+		Token t1 = new Token(1), t2 = new Token("/"), t3 = new Token(135);
 		
-		//has to be tokenStack? 
-		ArrayList<Token> t = new ArrayList<>(); //expecting : {"1","+","2","-","3"}
-		//actual
-		t = InputParser.getTokensFromInputString(inputString);
+		ArrayList<Token> inputTokens = new ArrayList<>();
+		inputTokens.add(t1); inputTokens.add(t2); inputTokens.add(t3);
 		
-		//expected
-		ArrayList<Token> arlt = new ArrayList<Token>();
-		Token t1 = new Token(1), t2 = new Token("+"), t3 = new Token(2), t4 = new Token("-"), t5 = new Token(3);
-		arlt.add(t1); arlt.add(t2); arlt.add(t3); arlt.add(t4); arlt.add(t5);
+		TokenStack ts = new TokenStack();
+		ts.push(t1); ts.push(t2); ts.push(t3);
 		
-		for (int i=0; i<=arlt.size()-1; i++){
-		assertTrue(equals(arlt.get(i).equals(t.get(i))));
+		boolean match = false;
+		ts.pop();
+		for (int i = inputTokens.size(); i == 0 ; i --){
+			if (inputTokens.contains(ts.top())){
+				ts.pop();
+				match = true;
+			}
 		}
-		assertTrue(t.equals(arlt));
-		assertTrue(t.containsAll(arlt));
-		assertTrue(t.size() == arlt.size());
-		
+		assertEquals(true, match);
 	}
+	
+	@Test
+	public void testInput(){
+		String[] args = {"1+", "2","-"," ","3"};
+		String inputString = null;
+		inputString = InputParser.formatingJoinedString(args); //"1+2-3"
+		assertEquals("1+2-3", inputString);
+	}
+	
+	@Test 
+	public void testInputParser_ReturnStringArr(){
+		String inputString = "1+2-3";
+		String[] expecting = {"1","+","2","-","3"};
+		String[] strArr = InputParser.formatStringToStrArr(inputString);
+		assertArrayEquals(expecting, strArr);
+
+	}
+
 
 }
  

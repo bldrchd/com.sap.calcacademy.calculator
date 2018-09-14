3cf7b8f6ec1e0d83c2bc3213f4983a5289179043
i311648 <asya.chakarova@sap.com>
2018-09-15 01:22:28 +0300

    negative numbers

--- src/newCalc/InputDetermination.java
+++ src/newCalc/InputDetermination.java
@@ -5,22 +5,25 @@ import java.util.ArrayList;
 public class InputDetermination {
 
 	private ArrayList<Object> operators = new ArrayList<Object>();
-	//private TokenStack operators = new TokenStack();
 	private ArrayList<Object> expression = new ArrayList<Object>();
+	private StringBuilder sb = new StringBuilder();
+	private Double digit = 0.0;
+	double valueSign = 1;
 
 	void determinate(String inputString) {
-		StringBuilder sb = new StringBuilder();
+
 		int countLeft = 0, countRight = 0;
-		Double digit = 0.0;
-		double valueSign = 1;
 
 		if (inputString.charAt(0) == ')') {
 			System.out.println("ERR - starts with )");
 			System.exit(1);
 		}
 
-		for (int i = 0; i <= inputString.length()-1; ++i) {
+		for (int i = 0; i <= inputString.length() - 1; ++i) {
+
 			char charAtIndex = inputString.charAt(i);
+			char nextCharAtIndex = inputString.charAt(i + 1);
+			boolean isPotentialCharacter = (!Character.isDigit(charAtIndex) || i != inputString.length());
 
 			if (charAtIndex != ')') {
 				if (charAtIndex == '(') {
@@ -28,86 +31,47 @@ public class InputDetermination {
 				}
 				if (Character.isDigit(charAtIndex)) {
 					sb.append(charAtIndex);
-				} else if (!Character.isDigit(charAtIndex) || i != inputString.length()) {
-					if (charAtIndex == '-') {
-						valueSign = -1; //TODO negative signs
-					} 
-					if (charAtIndex == '.' ) { // , - not allowed?
-						if (!Character.isDigit(inputString.charAt(i-1))) {
-							sb.append(0+charAtIndex);
+				} else if (isPotentialCharacter) {
+					if (!Character.isDigit(nextCharAtIndex)
+							|| i + 1 != inputString.length() && nextCharAtIndex == '-') {
+						valueSign = -1;
+					}
+					if (charAtIndex == '.') { // , - not allowed?
+						if (!Character.isDigit(inputString.charAt(i - 1))
+								&& matchesOperatorSymbol(inputString.charAt(i - 1))) {
+							sb.append(0 + charAtIndex);
 						}
 						sb.append(charAtIndex);
-						if (!Character.isDigit(inputString.charAt(i+1)) && Character.isDigit(inputString.charAt(i-1))) { //  e.g. 2.
+						if (!Character.isDigit(nextCharAtIndex) && Character.isDigit(inputString.charAt(i - 1))
+								&& matchesOperatorSymbol(nextCharAtIndex)) { // e.g: 2.+1 -> 2.0+1
 							sb.append(0);
 						}
-					} 
-					// DIGIT
-					if (sb.length() != 0) {
-						try {
-							digit = Double.valueOf(sb.toString());
-							System.out.println(valueSign*digit.doubleValue());
-							expression.add(valueSign*digit.doubleValue());
-							sb.setLength(0);
-						} catch (NumberFormatException nfe) {
-							System.err.println("Cannot convert to Double"); 
-						}
-					} /*
-						 * else { System.err.println("Empty builder"); }
-						 */
-					// OPERATOR
+					}
+					addValueToSB();
 					if (matchesOperatorSymbol(charAtIndex)) {
 						System.out.println(charAtIndex);
-						//Token operator = new Token(charAtIndex);
 						operators.add(charAtIndex);
-						//operators.push(operator);
-					} /*
-						 * else { System.err.println("Found " + charAtIndex +
-						 * " in the expression."); //not an error }
-						 */
-				} /*
-					 * else { System.err.println("WTF"); }
-					 */
-			} else if (charAtIndex == ')') { // TODO check empty
-				countRight++;
-				if (sb.length() != 0) {
-					try {
-						digit = Double.valueOf(sb.toString());
-						System.out.println(digit.doubleValue());
-						expression.add(digit.doubleValue());
-						sb.setLength(0);
-					} catch (NumberFormatException nfe) {
-						System.err.println("Cannot convert to Double"); 
 					}
 				}
+			} else if (charAtIndex == ')') { // TODO check empty
+				countRight++;
+				addValueToSB();
 				if (!operators.isEmpty()) {
-					String operatorToString; //= String.valueOf(operators.top().getOperator());
+					String operatorToString;
 					int lastItem = operators.lastIndexOf(operators);
-					operatorToString = (String) operators.get(lastItem); 
+					operatorToString = (String) operators.get(lastItem);
 					operators.remove(lastItem);
 					expression.add(operatorToString);
 				}
 			}
-			
+
 			if (i == inputString.length()) { // not needed check?
-				// addValueToExpressionString
-				if (sb.length() != 0) {
-					try {
-						digit = Double.valueOf(sb.toString());
-						System.out.println(digit.doubleValue());
-						expression.add(digit.toString());
-						sb.setLength(0);
-					} catch (NumberFormatException nfe) {
-						System.err.println("Cannot convert to Double");
-					}
-				} /*
-					 * else { System.err.println("Empty builder"); }
-					 */
+				addValueToSB();
 			}
 		}
 
 		if (countLeft == countRight) {
 			while (!operators.isEmpty()) {
-			//while (operators.top() != null) {
 				addOperatorToString();
 			}
 			buildFinalPostfixExpression();
@@ -116,21 +80,33 @@ public class InputDetermination {
 		}
 	}
 
-	void addValueToExpressionString() {
-		// TODO
+	void addValueToSB() {
+		if (sb.length() != 0) {
+			try {
+				digit = Double.valueOf(sb.toString());
+				System.out.println(valueSign * digit.doubleValue());
+				expression.add(valueSign * digit.doubleValue());
+				sb.setLength(0);
+			} catch (NumberFormatException nfe) {
+				System.err.println("Cannot convert to Double");
+			}
+		}
 	}
 
 	void addOperatorToString() {
 		if (!operators.isEmpty()) {
-			String operatorToString; //= String.valueOf(operators.top().getOperator());
+			String operatorToString; // =
+										// String.valueOf(operators.top().getOperator());
 			int lastItem = operators.lastIndexOf(operators);
-			operatorToString = (String) operators.get(lastItem); 
+			operatorToString = (String) operators.get(lastItem);
 			operators.remove(lastItem);
 			expression.add(operatorToString);
-			
-/*			String operatorToString = String.valueOf(operators.top().getOperator());
-			operators.removeFromStack();
-			expression.add(operatorToString);*/
+
+			/*
+			 * String operatorToString =
+			 * String.valueOf(operators.top().getOperator());
+			 * operators.removeFromStack(); expression.add(operatorToString);
+			 */
 		}
 	}
 
@@ -140,7 +116,7 @@ public class InputDetermination {
 		for (int i = 0; i <= expression.size(); i++) {
 			postfixExpression[i] = expression.get(i).toString();
 		}
-		//String postfixExpression = st.toString(); // TODO check
+		// String postfixExpression = st.toString(); // TODO check
 		System.out.println(postfixExpression);
 		return postfixExpression;
 	}

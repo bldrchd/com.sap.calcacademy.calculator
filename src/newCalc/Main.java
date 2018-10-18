95e584c15576be503f708da74f4527581596066b
i311648 <asya.chakarova@sap.com>
2018-10-18 15:24:53 +0300

    Moving buildString() to Main class

--- src/newCalc/Main.java
+++ src/newCalc/Main.java
@@ -3,7 +3,22 @@ package newCalc;
 public class Main {
 
     public static void main(String[] args) {
+        String buildedString = buildString(args);
         Calculator c = new Calculator();
-        System.out.println("MAIN RESULT: " + c.calculate(args));
+        System.out.println("MAIN RESULT: " + c.calculate(buildedString));
+    }
+
+    /**
+     * Returns user input arguments as one string
+     */
+    // TODO - ? The static here (because main is also static)
+    private static String buildString(String[] inputArgs) {
+        StringBuilder builder = new StringBuilder();
+        if (inputArgs != null) {
+            for (String s : inputArgs) {
+                builder.append(s);
+            }
+        }
+        return builder.toString();
     }
 }

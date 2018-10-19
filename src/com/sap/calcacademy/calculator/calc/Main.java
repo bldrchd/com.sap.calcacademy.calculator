a0aa368e592f2fc6046dc410e3f590488b7d814a
i311648 <asya.chakarova@sap.com>
2018-10-19 15:58:20 +0300

    exception handling

--- src/com/sap/calcacademy/calculator/calc/Main.java
+++ src/com/sap/calcacademy/calculator/calc/Main.java
@@ -8,25 +8,16 @@ public class Main {
             Calculator c = new Calculator();
             System.out.println("MAIN RESULT: " + c.calculate(buildedString));
         } catch (IllegalArgumentException iae) {
-            System.out.println(iae.getMessage());
-            System.out.println(iae.getCause()); // TODO - Why getCause() is null
-                                                // ?
-
-        } catch (NullPointerException npe) {
-            System.out.println(npe.getMessage());
+            System.err.println(iae.getMessage());
         }
     }
 
     /**
      * Returns user input arguments as one string
      */
-    // TODO - ? The static here (because main is also static)
     private static String buildString(String[] inputArgs) throws IllegalArgumentException, NullPointerException {
         StringBuilder builder = new StringBuilder();
-        if (inputArgs == null) {
-            throw new NullPointerException("Argumets are null.");
-        }
-        if (inputArgs.length == 0) {
+        if (inputArgs == null || inputArgs.length == 0) {
             throw new IllegalArgumentException("There are no arguments.");
         }
         for (String s : inputArgs) {
